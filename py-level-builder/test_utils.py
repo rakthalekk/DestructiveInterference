d083f6a88dd76f079ee7c77d07e1d3fad6145b55
melodysium <melodysium@gmail.com>
2026-04-19 03:08:36 -0400

    test: sanity check unit tests for py-level-builder scripts

---
+++ py-level-builder/test_utils.py
@@ -0,0 +1,50 @@
+import unittest
+
+from utils import ratio_to_A4
+
+
+class RatioToA4Test(unittest.TestCase):
+
+    def test_ratio_to_A4_cases(self):
+
+        # A4 * 1.0 = A4
+        self.assertAlmostEqual(
+            ratio_to_A4(69),
+            1.0
+        )
+
+        # A4 * 2.0 = A5
+        self.assertAlmostEqual(
+            ratio_to_A4(81),
+            2.0
+        )
+
+        # A4 x 0.5 = A3
+        self.assertAlmostEqual(
+            ratio_to_A4(57),
+            0.5
+        )
+
+        # A4 x ... = C5
+        self.assertAlmostEqual(
+            ratio_to_A4(72),
+            523.2511306011972 / 440.0
+        )
+
+        # A4 x ... = G9
+        self.assertAlmostEqual(
+            ratio_to_A4(127),
+            12543.85 / 440.0,
+            places=4 # more sig figs left of decimal, so it's fine
+        )
+
+        # A4 x ... = C1
+        self.assertAlmostEqual(
+            ratio_to_A4(24),
+            32.7032 / 440.0,
+        )
+
+
+
+if __name__ == '__main__':
+    unittest.main()
